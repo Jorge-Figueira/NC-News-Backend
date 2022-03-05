af54376f04bade78a6437c01465c414ad43afd2c
Jorge-Figueira <xurxo.figueira@gmail.com>
2022-03-05 18:29:04 +0000

    seeded env

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+console.log(ENV, "<<<env ")
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -22,27 +23,3 @@ const config =
 module.exports = new Pool(config);
 
 
-// const { Pool } = require('pg');
-
-// const ENV = process.env.NODE_ENV || 'development';
-// require('dotenv').config({
-  
-//   path: `${__dirname}/../.env.${ENV}`,
-// });
-
-
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//   throw new Error('PGDATABASE or DATABASE_URL not set');
-// }
-
-// const config =
-//   ENV === 'production'
-//     ? {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//           rejectUnauthorized: false,
-//         },
-//       }
-//     : {};
-
-// module.exports = new Pool();
